11861adeddd6447c7de1671ef8d71983cc01ae8e
sairamkarthik <sairamkarthik_babji@srmuniv.edu.in>
2017-07-15 11:20:23 +0530

    Create SimpleInterest.py

---
+++ SimpleInterest.py
@@ -0,0 +1,9 @@
+print("Enter the Principal Amount:")
+p=raw_input()
+print("Enter the Rate of Interest:")
+r=raw_input()
+print("Enter the Time Period:")
+t=raw_input()
+interest=int(p)*int(r)*int(t)/100
+print("The Simple Interest is")
+print(interest)
