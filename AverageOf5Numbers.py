5beb974fb8673d95eed5b80ea80ae819c6a3f191
sairamkarthik <sairamkarthik_babji@srmuniv.edu.in>
2017-07-15 11:31:09 +0530

    Create AverageOf5Numbers.py

---
+++ AverageOf5Numbers.py
@@ -0,0 +1,9 @@
+print("Enter any five numbers:")
+a=raw_input()
+b=raw_input()
+c=raw_input()
+d=raw_input()       #average of 5 numbers
+e=raw_input()
+avg=(int(a)+int(b)+int(c)+int(d)+int(e))/5
+print("The average is")
+print(avg)
