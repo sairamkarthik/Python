7b3939cdd0e87f1e7c249b60a641ff99b71e1bfb
sairamkarthik <sairamkarthik_babji@srmuniv.edu.in>
2017-07-23 09:20:47 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,15 @@
+# To find the factorial of a number provided by the user.
+
+
+num = int(input("Enter a number: "))
+
+factorial = 1
+
+if num < 0:
+   print "Sorry, factorial does not exist for negative numbers" 
+elif num == 0:
+   print "The factorial of 0 is 1"
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print "\nThe factorial of is: ",factorial
